2f22b6c56ee42d347341b818283e60d39025fbd9
Daniel P <pittarodaniel@gmail.com>
2022-07-05 21:23:53 -0300

    feat: :recycle: criação pasta routes, realocando código
    criando pasta routes e adicionando um novo arquivo a ela, contendo a api, testada e funcionando como esperado pelo arquivo espress-static.js

--- express-static.js
+++ express-static.js
@@ -1,58 +1,10 @@
 const express = require('express');
+const router = require('./routes');
 const app = express();
 
 app.use('/', express.static(__dirname + "/static"));
+app.use('/posts', router)
 
 app.listen(4000, () => {
     console.log('server listen on http://localhost:4000');
 })
-
-const bodyParser = require('body-parser')
-const router = express.Router();
-
-app.use(bodyParser.json())
-
-router.route("/")
-    .all((req, res, next) => {
-        console.log(`Request from : ${req.originalUrl}`)
-        console.log(`Request type : ${req.method}`)
-        console.log(`Request params : ${req.params}`)
-        next();
-    })
-    .get((req, res) => {
-        console.log(req.params)
-        res.send(JSON.stringify([]))
-        res.status(200)
-        res.end()
-    })
-    .post((req, res) => {
-        console.log(JSON.stringify(req.body))
-        res.status(201)
-        res.end()
-    })
-
-router.param('id', (req, res, next, id) => {
-        console.log(`Request from : ${req.params}`)
-        console.log(`Request type : ${req.method}`)
-        console.log(`Request id : ${id}`)
-        next()
-    })
-    .route("/:id")
-    .get((req, res) => {
-        console.log(req.params)
-        res.send(JSON.stringify(req.params))
-        res.status(200)
-        res.end()
-    })
-    .put((req, res) => {
-        console.log(req.body)
-        res.status(203)
-        res.end()
-    })
-    .delete((req, res) => {
-        res.status(203)
-        res.end()
-    })
-
-
-app.use('/posts', router)
